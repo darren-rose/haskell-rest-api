c1afbd32692c85de7fc2134ab512310b7d2ddb4f
darren-rose <darrenwrose@gmail.com>
2019-09-11 21:53:05 +0200

    add web request logging

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,12 @@ module Main where
 
 import Web.Scotty
 import Data.Monoid (mconcat)
+import Network.Wai.Middleware.RequestLogger
+
 import Lib
 
-main = scotty 3000 $
+main = scotty 3000 $ do
+  middleware logStdoutDev
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
