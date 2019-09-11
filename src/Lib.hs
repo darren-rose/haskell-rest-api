0a8dfcec21e0f4167c06220cc26b57d05d9bc02a
darren-rose <darrenwrose@gmail.com>
2019-09-11 21:13:29 +0200

    add some tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( someFunc,
+      multiplyByFive
     ) where
 
 import qualified Data.Text.IO as T
+
+multiplyByFive :: Integer -> Integer
+multiplyByFive x = x * 5
 
 someFunc :: IO ()
 someFunc = T.putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib.multiplyByFive" $ do
+    it "multiply 5 by 5" $ do
+      multiplyByFive 5 `shouldBe` (25 :: Integer)
+
+    it "multiply 7 by 5" $ do
+      multiplyByFive 7 `shouldBe` (35 :: Integer)
+
+    it "multiply 17 by 5" $ do
+      multiplyByFive 17 `shouldBe` (85 :: Integer)
